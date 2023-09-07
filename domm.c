c5a5c2fa029675af63b320419fb2e5e1c15cc89c
sredraert yannick <yellowyellowyellow2005@gmail.com>
2023-09-08 01:39:39 +0300

    Добавьте файлы проекта.

---
+++ domm.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+void main()
+
+{
+
+
+	puts("     /\\     ");
+	puts("    /  \\    ");
+	puts("   /    \\   ");
+	puts("  /______\\  ");
+	puts("  |      |");
+	puts("  |  __  |");
+	puts("  | |  | |");
+	puts("  | |__| |");
+	puts("  |      |");
+	puts("  |______|");
+
+
+}
+
